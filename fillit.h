e72ef352c724495d360ea5d697f882c4d9f05494
Tengisbold Batsaikhan <tebatsai@e1z2r7p4.42.us.org>
2019-06-12 03:23:11 -0700

    hey

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: tebatsai <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 15:55:03 by tebatsai          #+#    #+#             */
-/*   Updated: 2019/06/12 00:21:02 by tebatsai         ###   ########.fr       */
+/*   Updated: 2019/06/12 02:50:37 by tebatsai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,5 @@ int		isitvalidchar(char c);
 void	print_arr(char **s);
 int		count_hash(char *s);
 int		valid_file(char **s);
+int		face_hash(char *s);
 #endif
--- validatin.c
+++ validatin.c
@@ -6,7 +6,7 @@
 /*   By: tebatsai <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/11 10:34:36 by tebatsai          #+#    #+#             */
-/*   Updated: 2019/06/12 00:24:59 by tebatsai         ###   ########.fr       */
+/*   Updated: 2019/06/12 03:20:10 by tebatsai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,49 @@ int		count_hash(char *s)
 	}
 	return (i == 4) ? 1 : 0;
 }
+int 	dail(int i)
+{
+	if (i == 1000110001 || i == 10011001 || i == 1100011 || i == 11011)
+		return (1);
+	else if (i == 1111)
+		return (1);
+	else if (i == 1000111 || i == 10111 || i == 110010001 || i == 1100010001)
+		return (1);
+	else if (i == 100111 || i == 100011001 || i == 111001 || i == 100110001)
+		return (1);
+	else if (i == 110011)
+		return (1);
+	else
+		return (0);
+}
+int		face_hash(char *s)
+{
+	int i;
+	int z;
+	char *c;
+
+	c = "#...#...#...#";
+	z = 0;
+	i = 0;
+	if (!ft_strncmp(s, c, ft_strlen(c)))
+		return (1);
+	while (*s)
+	{
+		if(*s == '#')
+		{
+			if (z > 0)
+				i *= 10;
+			i++;
+			z++;
+		}
+		else if (z == 4)
+			return(dail(i));
+		else if (*s == '.')
+			i *= 10;
+		s++;
+	}
+	return (0);
+}
 
 int		valid_file(char **s)
 {
@@ -71,7 +114,7 @@ int		valid_file(char **s)
 	i = 0;
 	while (s[i])
 	{
-		if (!(count_hash(s[i])))
+		if (!(count_hash(s[i])) || !(face_hash(s[i])))
 			return (0);
 		i++;
 	}
